6f43c830f90c2d9167e74ced58783ec5fa6ecba2
Mário Travassos <retchut@protonmail.ch>
2021-05-25 04:38:20 +0100

    feat: QoL interface upgrades

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,11 @@ void insertNewAirport(Graph g){
     std::string confirm = "";
 
     do {
-        std::cout << "Please introduce the airport's latitude\n";
+        std::cout << "Please input the airport's latitude\n";
         std::cin >> latitude;
-        std::cout << "Please introduce the airport's longitude\n";
+        std::cout << "Please input the airport's longitude\n";
         std::cin >> longitude;
-        std::cout << "Finally, introduce the airport's 3 letter IATA code name\n";
+        std::cout << "Finally, input the airport's 3 letter IATA code name\n";
         std::getline(std::cin, name);
 
         std::cout << "You're about to create an airport with the following characteristics:\nid: " << id
@@ -41,11 +41,11 @@ void insertNewConnection(Graph g){
         std::string confirm = "";
 
         do{
-            std::cout << "Please introduce the connection's source airport's ID\n";
+            std::cout << "Please input the connection's source airport's ID\n";
             std::cin >> source;
-            std::cout << "Please introduce the connection's destination airport's ID\n";
+            std::cout << "Please input the connection's destination airport's ID\n";
             std::cin >> destination;
-            std::cout << "Please introduce the connection's distance\n";
+            std::cout << "Please input the connection's distance\n";
             std::cin >> distance;
 
             std::cout << "You're about to create a connection with the following characteristics:\nid: " << id
@@ -63,10 +63,32 @@ void insertNewConnection(Graph g){
 
 }
 
+int drawIberianDataSet(){;
+    std::cout << "Which dataset would you like to draw?\n";
+    std::cout << "1-Iberian Peninsula data set\n";
+    std::cout << "2-World data set\n";
+    std::cout << "0-Quit\n";
+
+    std::string input;
+
+    while(true){
+        std::getline(std::cin,input);
+        if(input == "1"){
+            return 1;
+        }
+        if(input == "2"){
+            return 2;
+        }
+        if(input == "0"){
+            return 0;
+        }
+    }
+}
+
 int readInput(Graph g){
     std::cout << "1-Insert Airport\n";
     std::cout << "2-Insert Connection\n";
-    std::cout << "3-Close menu and run program\n";
+    std::cout << "3-Stop making changes and run the program\n";
     std::cout << "0-Close the program\n";
 
     std::string input;
@@ -90,44 +112,135 @@ int readInput(Graph g){
     }
 }
 
-int getDisplayInput(){
+int getPassengerNum(){
 
-    std::cout << "Please insert the ID of the plane whose route you'd like to display on the graph viewer.\n";
+    std::cout << "Please input the maximum number of passengers to add to each airport (min 1).\n";
+    int num = 0;
+    std::string input;
+    std::string::size_type sz;
+    std::getline(std::cin, input);
+    try{
+        num = std::stoi(input, &sz);
+        if(num <= 0){
+            std::cout << "That amount is too low. The maximum was set to 20.\n";
+            return 20;
+        }
+        return num;
+    }
+    catch (...) {
+        std::cout << "Invalid input. The maximum was set to 20.\n";
+        return 20;
+    }
+}
+
+int getPlaneNum(){
+
+    std::cout << "Please input the number of planes to create (min 1).\n";
+    int num = 0;
+    std::string input;
+    std::string::size_type sz;
+    std::getline(std::cin, input);
+    try{
+        num = std::stoi(input, &sz);
+        if(num <= 0){
+            std::cout << "That amount is too low. 10 planes will be created.\n";
+            return 10;
+        }
+        return num;
+    }
+    catch (...) {
+        std::cout << "Invalid input. 10 planes will be created.\n";
+        return 10;
+    }
+}
+
+int getRplcNum(){
+
+    std::cout << "Please input the number of replacement crews to create (min 0 - disabled replacement).\n";
+    int num = 0;
+    std::string input;
+    std::string::size_type sz;
+    std::getline(std::cin, input);
+    try{
+        num = std::stoi(input, &sz);
+        if(num < 0){
+            std::cout << "That amount is too low. 0 replacement crews will be created.\n";
+            return 0;
+        }
+        return num;
+    }
+    catch (...) {
+        std::cout << "Invalid input. 0 replacement crews will be created.\n";
+        return 0;
+    }
+}
+
+int getTrackedPlane(int maxPlanes){
+
+    std::cout << "Please input the ID of the plane whose route you'd like to display on the graph viewer (min 1).\n";
     int id = 0;
     std::string input;
     std::string::size_type sz;
     std::getline(std::cin, input);
     try{
         id = std::stoi(input, &sz);
+        if(id < 0){
+            std::cout << "That amount is too low. The route of plane 0 will be displayed.\n";
+            return 0;
+        }
+        else if(id > maxPlanes){
+            std::cout << "That id does not exist. The route of plane 0 will be displayed.\n";
+            return 0;
+        }
         return id;
     }
     catch (...) {
-        std::cout << "Invalid input. The route of plane 1 will be displayed.";
-        return 1;
+        std::cout << "Invalid input. The route of plane 1 will be displayed.\n";
+        return 0;
     }
 }
 
 int main() {
     std::cout << "-----Welcome to FightNet!-----\n\n";
-    Graph g;
-    //loader(g,"../airports_datasets/airports_full/",  "", ".txt");
-    loader(g, "../airports_datasets/airports_iberia/", "", ".txt");
 
     while(true){
-        int input = readInput(g);
-        if(input == 0)
-            return 0;
-        else if(input == 3)
-            break;
-    }
-    
-    int planeToDisplay = getDisplayInput();
+        Graph g;
+        switch(drawIberianDataSet()){
+            case 1:
+                loader(g, "../airports_datasets/airports_iberia/", "", ".txt");
+                break;
+            case 2:
+                loader(g,"../airports_datasets/airports_full/",  "", ".txt");
+                break;
+            case 0:
+                return 0;
+        }
 
-    g.generatePassengers(15);
-    g.generatePlanes(5);
-    g.generateReplacementCrews(0);
-    g.calculateSteps();
-    g.printRoutes();
-    g.viewGraph("", planeToDisplay);
-    return 0;
+        while(true){
+            int input = readInput(g);
+
+            //display graph
+            if(input == 3){
+                break;
+            }
+            //end execution of program
+            else if(input == 0){
+                return 0;
+            }
+            //keep altering the graph
+        }
+
+        int passengerNum = getPassengerNum();
+        int planeNum = getPlaneNum();
+        int replacementNum = getRplcNum();
+        int trackedPlaneID = getTrackedPlane(planeNum);
+
+        g.generatePassengers(passengerNum);
+        g.generatePlanes(planeNum);
+        g.generateReplacementCrews(replacementNum);
+        g.calculateSteps();
+        g.printRoutes();
+        g.viewGraph("", trackedPlaneID);
+    }
+
 }
